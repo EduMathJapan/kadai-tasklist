af578ad65648c973442dbd22ff14bd59deec60df
furu <education.in.sns@gmail.com>
2021-05-17 15:29:54 +0000

    tasklist add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,11 +4,18 @@
 
 <!-- ここにページ毎のコンテンツを書く -->
 
+
+
 <h1>タスクの登録</h1>
 
 <div class = 'row'>
     <div class='col-6'>
         {!! Form::model($task, ['route'=>'tasks.store']) !!}
+        
+            <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
         
             <div class = 'form-group'>
                 {!! Form::label('content','タスク:') !!}
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -11,6 +11,7 @@
         <thead>
             <tr>
                 <th>id</th>
+                <th>ステータス</th></th>
                 <th>タスク</th>
             </tr>
         </thead>
@@ -18,6 +19,7 @@
             @foreach($tasks as $task)
             <tr>
                 <td>{!! link_to_route('tasks.show', $task ->id, ['task'=> $task->id]) !!}</td>
+                <td>{{ $task->status }}</td>
                 <td>{{ $task->content }}</td>
             </tr>
             @endforeach
@@ -26,10 +28,12 @@
     
     @endif
     
-    {{-- タスク作成へのリンク --}}
+ 
+    
+
     
 {!! link_to_route('tasks.create', '新規タスクの登録',[],['class' =>'btn btn-primary']) !!}
-
+ {!! link_to_route( 'tasks.edit' , 'このタスクを編集' , ['task' =>$task->id], ['class' => 'btn btn-light',]) !!}
 <div class ='collapse navbar-collapse' id='nav-bar'>
     <ul class = 'navbar-nav mr-auto'></ul>
     <ul class= 'navbar-nav'>
